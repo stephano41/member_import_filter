ae39ae04d1819257e1529b1935552a2c0e832a65
Steven <stevenzhangshao@gmail.com>
2023-01-14 17:15:34 +1030

    settings page

--- admin/class-member_import_filter-admin.php
+++ admin/class-member_import_filter-admin.php
@@ -51,6 +51,8 @@ class Member_import_filter_Admin {
 
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
+		add_action('admin_menu', array( $this, 'addPluginAdminMenu' ), 9);    
+		add_action('admin_init', array( $this, 'registerAndBuildFields' ));
 
 	}
 
@@ -100,4 +102,175 @@ class Member_import_filter_Admin {
 
 	}
 
+	public function addPluginAdminMenu() {
+		//add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
+		add_menu_page(  $this->plugin_name, 'User Import Filter', 'manage_options', $this->plugin_name, array( $this, 'displayPluginAdminSettings' ) );
+	}
+
+
+	public function displayPluginAdminSettings() {
+		// set this var to be used in the settings-display view
+		$active_tab = isset( $_GET[ 'tab' ] ) ? $_GET[ 'tab' ] : 'general';
+		if(isset($_GET['error_message'])){
+			add_action('admin_notices', array($this,'SettingsMessages'));
+			do_action( 'admin_notices', $_GET['error_message'] );
+		}
+		require_once 'partials/'.$this->plugin_name.'-admin-settings-display.php';
+	}
+	
+	
+	public function SettingsMessages($error_message){
+		switch ($error_message) {
+			case '1':
+				$message = __( 'There was an error adding this setting. Please try again.  If this persists, shoot us an email.', 'my-text-domain' );                 
+				$err_code = esc_attr( 'plugin_name_example_setting' );                 
+				$setting_field = 'plugin_name_example_setting';                 
+				break;
+		}
+		$type = 'error';
+		add_settings_error(
+				$setting_field,
+				$err_code,
+				$message,
+				$type
+			);
+	}
+
+
+	public function registerAndBuildFields() {
+		/**
+	   * First, we add_settings_section. This is necessary since all future settings must belong to one.
+	   * Second, add_settings_field
+	   * Third, register_setting
+	   */     
+		add_settings_section(
+		// ID used to identify this section and with which to register options
+		'user_import_setting_section', 
+		// Title to be displayed on the administration page
+		'',  
+		// Callback used to render the description of the section
+		array( $this, 'display_general_description' ),    
+		// Page on which to add this section of options
+		'general_settings'                   
+		);
+		unset($args);
+		$args = array (
+				'type'      => 'input',
+				'subtype'   => 'text',
+				'id'    => 'default_role',
+				'name'      => 'default_role',
+				'required' => 'false',
+				'get_options_list' => '',
+				'value_type'=>'normal',
+				'wp_data' => 'option'
+			);
+		add_settings_field(
+			'default_role',
+			'Default import role ID',
+			array( $this, 'render_settings_field' ),
+			'general_settings',
+			'user_import_setting_section',
+			$args
+		);
+	
+	
+		register_setting(
+			'general_settings',
+			'default_role'
+			);
+		
+		
+		unset($args);
+		$args = array (
+				'type'      => 'input',
+				'subtype'   => 'text',
+				'id'    => 'metadata_column_names',
+				'name'      => 'metadata_column_names',
+				'required' => 'false',
+				'get_options_list' => '',
+				'value_type'=>'normal',
+				'wp_data' => 'option'
+			);
+
+		add_settings_field(
+			'metadata_column_names',
+			'Column names to include as metadata only',
+			array( $this, 'render_settings_field' ),
+			'general_settings',
+			'user_import_setting_section',
+			$args
+		);
+	
+	
+		register_setting(
+			'general_settings',
+			'metadata_column_names'
+			);
+
+	
+	}
+
+
+	public function display_general_description() {
+		echo '<p>These settings apply to all Plugin Name functionality.</p>';
+	  } 
+	
+	
+	public function render_settings_field($args) {
+	/* EXAMPLE INPUT
+				'type'      => 'input',
+				'subtype'   => '',
+				'id'    => $this->plugin_name.'_example_setting',
+				'name'      => $this->plugin_name.'_example_setting',
+				'required' => 'required="required"',
+				'get_option_list' => "",
+				'value_type' = serialized OR normal,
+				'wp_data'=>(option or post_meta),
+				'post_id' =>
+		*/     
+		if($args['wp_data'] == 'option'){
+			$wp_data_value = get_option($args['name']);
+		} elseif($args['wp_data'] == 'post_meta'){
+			$wp_data_value = get_post_meta($args['post_id'], $args['name'], true );
+		}
+	
+		switch ($args['type']) {
+	
+			case 'input':
+				$value = ($args['value_type'] == 'serialized') ? serialize($wp_data_value) : $wp_data_value;
+				switch($args['subtype']){
+					case 'checkbox':
+						$checked = ($value) ? 'checked' : '';
+						echo '<input type="'.$args['subtype'].'" id="'.$args['id'].'" "'.$args['required'].'" name="'.$args['name'].'" size="40" value="1" '.$checked.' />';
+						break;
+					case 'text':
+						$this->render_text_field($args, $value);
+
+						/*<input required="required" '.$disabled.' type="number" step="any" id="'.$this->plugin_name.'_cost2" name="'.$this->plugin_name.'_cost2" value="' . esc_attr( $cost ) . '" size="25" /><input type="hidden" id="'.$this->plugin_name.'_cost" step="any" name="'.$this->plugin_name.'_cost" value="' . esc_attr( $cost ) . '" />*/
+						break;
+				
+					default:
+						break;
+					}
+			default:
+				# code...
+				break;
+		}
+	}
+
+	public function render_text_field($args, $value){
+		$prependStart = (isset($args['prepend_value'])) ? '<div class="input-prepend"> <span class="add-on">'.$args['prepend_value'].'</span>' : '';
+		$prependEnd = (isset($args['prepend_value'])) ? '</div>' : '';
+		$step = (isset($args['step'])) ? 'step="'.$args['step'].'"' : '';
+		$min = (isset($args['min'])) ? 'min="'.$args['min'].'"' : '';
+		$max = (isset($args['max'])) ? 'max="'.$args['max'].'"' : '';
+		if(isset($args['disabled'])){
+			// hide the actual input bc if it was just a disabled input the informaiton saved in the database would be wrong - bc it would pass empty values and wipe the actual information
+			echo $prependStart.'<input type="'.$args['subtype'].'" id="'.$args['id'].'_disabled" '.$step.' '.$max.' '.$min.' name="'.$args['name'].'_disabled" size="40" disabled value="' . esc_attr($value) . '" /><input type="hidden" id="'.$args['id'].'" '.$step.' '.$max.' '.$min.' name="'.$args['name'].'" size="40" value="' . esc_attr($value) . '" />'.$prependEnd;
+		} else {
+			echo $prependStart.'<input type="'.$args['subtype'].'" id="'.$args['id'].'" "'.$args['required'].'" '.$step.' '.$max.' '.$min.' name="'.$args['name'].'" size="40" value="' . esc_attr($value) . '" />'.$prependEnd;
+		}
+
+
+	}
 }
--- member_import_filter.php
+++ member_import_filter.php
@@ -31,65 +31,112 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-add_filter('is_iu_import_userdata', 'mod_userdata', 0, 2);
-add_filter('is_iu_import_usermeta', 'mod_usermeta', 0, 2);
 
-function mod_userdata($userdata, $usermeta){
-	error_log(print_r($usermeta, TRUE));
-	error_log(print_r($userdata, TRUE));
-
-	$display_name = $usermeta['Full Name'];
-
-	if (array_key_exists("Membership Email Address", $usermeta)){
-		$user_email = $usermeta['Membership Email Address'];		
-	} elseif (array_key_exists("Ticket Email Address", $usermeta)){
-		$user_email = $usermeta['Ticket Email Address'];		
-	} else{
-		throw new \Exception(implode($usermeta));
-	}
-
-	$modded_userdata = array(
-		"user_login" => str_replace(' ', '', $display_name),
-		"user_email" => $user_email,
-		"user_pass" => null, 
-		"first_name" => explode(" ", $display_name)[0],
-		"last_name" => end(explode(" ", $display_name)),
-		"display_name" => $display_name,
-		"role" => "um_member");
-
-	return $modded_userdata;
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-member_import_filter-activator.php
+ */
+function activate_member_import_filter() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-member_import_filter-activator.php';
+	Member_import_filter_Activator::activate();
 }
 
-
-function mod_usermeta($usermeta, $userdata){
-	$remove = ['Ticket ID',
-				'Order ID',
-				'Total Paid',
-				'Mobile Number',
-				'Scanned',
-				'Payment',
-				'Student ID',
-				'Table Number',
-				'Purchased',
-				'Updated',
-				'Dietary Requirements',
-				'Student Number',
-				'standard_studyYear',
-				'Membership Type',
-				'Price Paid',	
-				'Payment Method',
-				'University',
-				'Year Level',
-				'Status'];
-
-	return remove_elements($remove, $usermeta);
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-member_import_filter-deactivator.php
+ */
+function deactivate_member_import_filter() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-member_import_filter-deactivator.php';
+	Member_import_filter_Deactivator::deactivate();
 }
 
-function remove_elements($remove, $original_array){
-	foreach($remove as $key => $value){
-		if (array_key_exists($key, $original_array)){
-			unset($original_array[$key]);
+register_activation_hook( __FILE__, 'activate_member_import_filter' );
+register_deactivation_hook( __FILE__, 'deactivate_member_import_filter' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-member_import_filter.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_member_import_filter() {
+
+	$plugin = new Member_import_filter();
+	$plugin->run();
+
+	add_filter('is_iu_import_userdata', 'mod_userdata', 0, 2);
+	add_filter('is_iu_import_usermeta', 'mod_usermeta', 0, 2);
+
+	function mod_userdata($userdata, $usermeta){
+	
+		$display_name = $usermeta['Full Name'];
+
+		if (array_key_exists("Membership Email Address", $usermeta)){
+			$user_email = $usermeta['Membership Email Address'];		
+		} elseif (array_key_exists("Ticket Email Address", $usermeta)){
+			$user_email = $usermeta['Ticket Email Address'];		
+		} else{
+			throw new \Exception(implode($usermeta));
 		}
+
+		$display_name_array = explode(" ", $display_name);
+
+		$modded_userdata = array(
+			"user_login" => str_replace(' ', '', $display_name),
+			"user_email" => $user_email,
+			"user_pass" => null, 
+			"first_name" => $display_name_array[0],
+			"last_name" => end($display_name_array),
+			"display_name" => $display_name,
+			"nickname" => $display_name,
+			"role" => get_option('default_role', 'subscriber'));
+
+		return $modded_userdata;
 	}
-	return $original_array;
+
+
+	function mod_usermeta($usermeta, $userdata){
+		$result = array_intersect_key($usermeta, array_flip(get_metadata_columns()));
+		error_log(print_r($result, TRUE));
+		return $result;
+	}
+
+	function remove_elements($remove, $original_array){
+		foreach($remove as $key => $value){
+			if (array_key_exists($key, $original_array)){
+				unset($original_array[$key]);
+			}
+		}
+		return $original_array;
+	}
+
+	function get_metadata_columns(){
+		$metadata_column_names = get_option('metadata_column_names');
+
+		return explode(",", str_replace(", ", ",", $metadata_column_names));
+	}
+
 }
+run_member_import_filter();
+
+
+
+
+// add_action('admin_menu', 'add_setup_menu');
+
+// function add_setup_menu(){
+// 	add_menu_page('User Import Filter', 'User Import Filter', 'manage_options', 'user-import-filter', 'admin_init');
+// }
+
+// function admin_init(){
+// 	echo "<h1>Hello World!</h1>";
+// }
